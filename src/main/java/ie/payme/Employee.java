4d151279f8a74bfbef168f97a65836323b29d520
Marlowe <g00325096@gmit.ie>
2021-03-25 20:45:42 +0000

    start of the codes being put int

---
+++ src/main/java/ie/payme/Employee.java
@@ -0,0 +1,32 @@
+package ie.payme;
+
+public class Employee {
+
+    private String empName;
+    private String empId;
+
+    public Employee(String empName, String empId) {
+        this.empName = empName;
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+
+    public static Employee createEmp(String name, String empId){
+        return new Employee(name, empId);
+    }
+}
---
+++ src/test/java/ie/payme/EmployeeTest.java
@@ -0,0 +1,4 @@
+package ie.payme;
+
+public class EmployeeTest {
+}
